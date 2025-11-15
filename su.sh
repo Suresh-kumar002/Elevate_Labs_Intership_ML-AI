9d1656e923f9f39afe488dbe2bc9db248684ec31
Suresh kumar <officialsuru2983@gmail.com>
2025-11-15 19:42:24 +0530

    Add files via upload

---
+++ su.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter the nu "
+read num
+
+sum=0
+temp=$num
+
+while [ $temp -gt 0 ]
+do
+
+digit=$((temp % 10))
+sum=$((sum + digit * digit * digit))
+temp=$((temp / 10))
+done
+
+if [ $sum -eq $num ]
+then
+echo "$num is an Armstong number"
+else
+echo "$num is not Armstrong number"
+fi
